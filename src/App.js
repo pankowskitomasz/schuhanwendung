a52f08e2974e86f2bd409e12cf31ae1dba822882
pankowskitomasz <pankowski.email@gmail.com>
2025-04-01 06:22:09 +0200

    routing, layout: signin, signout, passwordupdate, restorepassword

--- src/App.js
+++ src/App.js
@@ -1,111 +1,59 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
-import AnwendungHeader from "./komponenten/anwendungHeader";
-import AnwendungFooter from "./komponenten/anwendungFooter";
 
-//Allgemeine
-import DatenschutzAnsicht from "./ansichten/allgemeine/datenschutzAnsicht";
-import EinloggenAnsicht from "./ansichten/allgemeine/einloggenAnsicht";
-import HomeAnsicht from "./ansichten/allgemeine/homeAnsicht";
-import KontaktAnsicht from "./ansichten/allgemeine/kontaktAnsicht";
-import NutzungsbedingungenAnsicht from "./ansichten/allgemeine/nutzungsbedingungenAnsicht";
-import RegistrierenAnsicht from "./ansichten/allgemeine/registrierenAnsicht";
-import WiederherstellenAnsicht from "./ansichten/allgemeine/wiederherstellenAnsicht";
-
-//Bestellungen
-import BestellungenAllgemeineAnsicht from "./ansichten/bestellungen/bestellungenAllgemeineAnsicht";
-import BestellungenBestatigenAnsicht from "./ansichten/bestellungen/bestellungenBestatigenAnsicht";
-import BestellungenLieferungAnsicht from "./ansichten/bestellungen/bestellungenLieferungAnsicht";
-import BestellungenWarenkorbAnsicht from "./ansichten/bestellungen/bestellungenWarenkorbAnsicht";
-import BestellungenZahlungAnsicht from "./ansichten/bestellungen/bestellungenZahlungAnsicht";
-
-//Kunde
-import KundeDashboardAnsicht from "./ansichten/kunde/kundeDashboardAnsicht";
-import KundeMeineBestellungenAnsicht from "./ansichten/kunde/kundeMeineBestellungenAnsicht";
-import KundeMeineDatenAnsicht from "./ansichten/kunde/kundeMeineDatenAnsicht";
-import KundeMeineZahlungenAnsicht from "./ansichten/kunde/kundeMeineZahlungenAnsicht";
-import KundeProfilAnsicht from "./ansichten/kunde/kundeProfilAnsicht";
-
-//Lagerung
-import LagerungAktulisierenArtikelAnsicht from "./ansichten/lagerung/lagerungAktualisierenArtikelAnsicht";
-import LagerungKategorienAnsicht from "./ansichten/lagerung/lagerungKategorienAnsicht";
-import LagerungNeuArtikelAnsicht from "./ansichten/lagerung/lagerungNeuArtikelAnsicht";
-import LagerungProduktenDetailsAnsicht from "./ansichten/lagerung/lagerungProduktenDetailsAnsicht";
-import LagerungProduktenListeAnsicht from "./ansichten/lagerung/lagerungProduktenListeAnsicht";
-
-//Mitarbeiter
-import MitarbeiterBestellungenAnsicht from "./ansichten/mitarbeiter/mitarbeiterBestellungenAnsicht";
-import MitarbeiterBestellungenDetailsAnsicht from "./ansichten/mitarbeiter/mitarbeiterBestellungenDetailsAnsicht";
-import MitarbeiterDashboardAnsicht from "./ansichten/mitarbeiter/mitarbeiterDashboardAnsicht";
-import MitarbeiterKundeDetailsAnsicht from "./ansichten/mitarbeiter/mitarbeiterKundeDetailsAnsicht";
-import MitarbeiterKundenAnsicht from "./ansichten/mitarbeiter/mitarbeiterKundenAnsicht";
-
-//Produkten
-import ProduktAktualisierenAnsicht from "./ansichten/produkten/produktAktualisierenAnsicht";
-import ProduktDetailsAnsicht from "./ansichten/produkten/produktDetailsAnsicht";
-import ProduktenListeAnsicht from "./ansichten/produkten/produktenListeAnsicht";
-import ProduktNeuAnsicht from "./ansichten/produkten/produktNeuAnsicht";
-import ProduktVerwaltenAnsicht from "./ansichten/produkten/produktVerwaltenAnsicht";
-
-//verwaltung
-import VerwaltungBenutzerAktualisierenAnsicht from "./ansichten/verwaltung/verwaltungBenutzerAktualisierenAnsicht";
-import VerwaltungBenutzerDashboardAnsicht from "./ansichten/verwaltung/verwaltungBenutzerDashboardAnsicht";
-import VerwaltungBenutzerGeschichteAnsicht from "./ansichten/verwaltung/verwaltungBenutzerGeschichteAnsicht";
-import VerwaltungBenutzerNeuAnsicht from "./ansichten/verwaltung/verwaltungBenutzerNeuAnsicht";
-import VerwaltungBenutzerVerwaltenAnsicht from "./ansichten/verwaltung/verwaltungBenutzerVerwaltenAnsicht";
+import AccountView from './views/accountView';
+import AccountCreateView from './views/account/accountCreateView';
+import AccountProfileView from './views/account/accountProfileView';
+import AccountUpdateView from './views/account/accountUpdateView';
+import CartView from './views/cartView';
+import CartCheckoutView from './views/cart/cartCheckoutView';
+import CartListView from './views/cart/cartListView';
+import CartPaymentView from './views/cart/cartPaymentView';
+import DashboardView from './views/dashboardView';
+import OrderView from './views/orderView';
+import OrderDetailsView from './views/order/orderDetailsView';
+import OrderListView from './views/order/orderListView';
+import PasswordUpdateView from './views/passwordUpdateView';
+import ProductView from './views/productView';
+import ProductDetailsView from './views/product/productDetailsView';
+import ProductListView from './views/product/productListView';
+import RestorePasswordView from './views/restorePasswordView';
+import SignInView from './views/signInView';
+import SignUpView from './views/signUpView';
+import WelcomeView from './views/welcomeView';
 
 import './App.css';
+
 
 function App() {
   return (
     <Router>
-        <AnwendungHeader/>
         <Routes>
-          <Route path="/datenschutz" element={<DatenschutzAnsicht/>} />
-          <Route path="/einloggen" element={<EinloggenAnsicht/>} />
-          <Route exact path="/" element={<HomeAnsicht/>} />
-          <Route path="/kontakt" element={<KontaktAnsicht />} />
-          <Route path="/Nutzungsbedingungen" element={<NutzungsbedingungenAnsicht />} />
-          <Route path="/registrieren" element={<RegistrierenAnsicht/>} />
-          <Route path="/wiederherstellen" element={<WiederherstellenAnsicht/>} />
-          
-          <Route exact path="/bestellungen" element={<BestellungenAllgemeineAnsicht/>}/>
-          <Route path="/bestellungen/bestatigen" element={<BestellungenBestatigenAnsicht/>} />
-          <Route path="/bestellungen/lieferung" element={<BestellungenLieferungAnsicht/>} />
-          <Route path="/bestellungen/warenkorb" element={<BestellungenWarenkorbAnsicht/>} />
-          <Route path="/bestellungen/zahlung" element={<BestellungenZahlungAnsicht/>} />
-          
-          <Route exact path="/kunde" element={<KundeDashboardAnsicht/>}/>
-          <Route path="/kunde/bestellungen" element={<KundeMeineBestellungenAnsicht/>} />
-          <Route path="/kunde/daten" element={<KundeMeineDatenAnsicht/>} />
-          <Route path="/kunde/zahlungen" element={<KundeMeineZahlungenAnsicht/>} />
-          <Route path="/kunde/profil" element={<KundeProfilAnsicht/>} />
-          
-          <Route exact path="/lagerung" element={<LagerungKategorienAnsicht/>}/>
-          <Route path="/lagerung/neu" element={<LagerungNeuArtikelAnsicht/>} />
-          <Route path="/lagerung/produkte/:id" element={<LagerungProduktenDetailsAnsicht/>} />
-          <Route path="/lagerung/liste/:id" element={<LagerungProduktenListeAnsicht/>} />
-          <Route path="/lagerung/aktulisieren/:id" element={<LagerungAktulisierenArtikelAnsicht/>} />
-          
-          <Route exact path="/mitarbeiter" element={<MitarbeiterDashboardAnsicht/>}/>
-          <Route path="/mitarbeiter/bestellungen" element={<MitarbeiterBestellungenAnsicht/>} />
-          <Route path="/mitarbeiter/bestellungen/:id" element={<MitarbeiterBestellungenDetailsAnsicht/>} />
-          <Route path="/mitarbeiter/kunden" element={<MitarbeiterKundenAnsicht/>} />
-          <Route path="/mitarbeiter/kunden/:id" element={<MitarbeiterKundeDetailsAnsicht/>} />
-          
-          <Route exact path="/produkten" element={<ProduktenListeAnsicht/>}/>
-          <Route path="/produkten/neu" element={<ProduktNeuAnsicht/>} />
-          <Route path="/produkten/:id" element={<ProduktDetailsAnsicht/>} />
-          <Route path="/produkten/aktualisieren/:id" element={<ProduktAktualisierenAnsicht/>} />
-          <Route path="/produkten/verwalten" element={<ProduktVerwaltenAnsicht/>} />
-
-          <Route exact path="/benutzer" element={<VerwaltungBenutzerDashboardAnsicht/>}/>
-          <Route path="/benutzer/aktualisieren/:id" element={<VerwaltungBenutzerAktualisierenAnsicht/>} />
-          <Route path="/benutzer/geschichte/:id" element={<VerwaltungBenutzerGeschichteAnsicht/>} />
-          <Route path="/benutzer/neu" element={<VerwaltungBenutzerNeuAnsicht/>} />
-          <Route path="/benutzer/verwalten" element={<VerwaltungBenutzerVerwaltenAnsicht/>} />
+            <Route exact path="/" element={<WelcomeView/>}/>
+            <Route exact path="/account" element={<AccountView/>}/>
+            <Route exact path="/account/create" element={<AccountCreateView/>}/>
+            <Route exact path="/account/profile" element={<AccountProfileView/>}/>
+            <Route exact path="/account/update" element={<AccountUpdateView/>}/>
+            <Route exact path="/cart" element={<CartView/>}/>
+            <Route exact path="/cart/checkout" element={<CartCheckoutView/>}/>
+            <Route exact path="/cart/list" element={<CartListView/>}/>
+            <Route exact path="/cart/payment" element={<CartPaymentView/>}/>
+            <Route exact path="/dashboard" element={<DashboardView/>}/>
+            <Route exact path="/order" element={<OrderView/>}/>
+            <Route exact path="/order/details/*">
+              <Route path=":id" element={<OrderDetailsView/>}/>
+            </Route>
+            <Route exact path="/order/list" element={<OrderListView/>}/>
+            <Route exact path="/product" element={<ProductView/>}/>
+            <Route exact path="/product/details*">
+              <Route path=":id" element={<ProductDetailsView/>}/>
+            </Route>
+            <Route exact path="/product/list" element={<ProductListView/>}/>
+            <Route exact path="/restore-password" element={<RestorePasswordView/>}/>
+            <Route exact path="/update-password" element={<PasswordUpdateView/>}/>
+            <Route exact path="/signin" element={<SignInView/>}/>
+            <Route exact path="/signup" element={<SignUpView/>}/>
         </Routes>
-        <AnwendungFooter/>
     </Router>
   );
 }
